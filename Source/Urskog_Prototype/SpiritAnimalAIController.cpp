d8536c0cb6feb72200e65e7df78bf3155a886b9b
Jari <madcook86@gmail.com>
2021-04-08 13:13:06 +0200

    Spirit animal navigation

---
+++ Source/Urskog_Prototype/SpiritAnimalAIController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpiritAnimalAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASpiritAnimalAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASpiritAnimalAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
